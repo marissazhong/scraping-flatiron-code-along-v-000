21d7ed23df30391188b33a8107bd51d37cb1978b
Marissa Zhong <mwz2101@columbia.edu>
2018-06-29 14:54:44 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -41,7 +41,7 @@ class Scraper
 
   def make_courses
     get_courses.each {|course|
-      if course.css(".box") != ""
+      if course.css(".box") != nil
         new_course = Course.new
         new_course.title = course.css("h2").text
         new_course.schedule = course.css(".date").text
