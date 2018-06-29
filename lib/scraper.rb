6f5f51226f4a1fcdbf1b4f1e579ead12f4dae81c
Marissa Zhong <mwz2101@columbia.edu>
2018-06-29 14:39:19 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -18,8 +18,10 @@ class Scraper
 
   def get_page
     html = open("http://learn-co-curriculum.github.io/site-for-scraping/courses")
+    doc = Nokogiri::HTML(html)
   end
 
-  
+  def get_courses
+    
 
 end
