91f12744ccd6dfd2726f1e675584d85dc0464d6f
Marissa Zhong <mwz2101@columbia.edu>
2018-06-29 15:32:14 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -48,7 +48,7 @@ class Scraper
   def make_courses
     get_courses.each {|course|
       #puts course
-      puts get_page.css(".post" && ".empty-event")[0]
+      #puts get_page.css(".post" && ".empty-event")[0]
       if course == get_page.css(".post" && ".empty-event")[0]
         #puts course
         new_course = Course.new
